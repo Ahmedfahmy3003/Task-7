69af5237293bfa71d5042a3fbfb39b43d2527e94
Ahmed Fahmy <161507571+Ahmedfahmy3003@users.noreply.github.com>
2024-04-30 01:56:34 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var name= "ahmed"
+var age= 24
+console.log("m name is "+ name +" my age is "+ age )
+console.log(`my name is :: ${name} and my age is ---> ${age}  ${5+6}`)
+
+
+// ''    ""    ``
+
+
